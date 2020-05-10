442eb89ab88da4cb75bdd1542163f4ae3f2f479f
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2020-05-10 19:23:20 +0200

    Force colored compiler output
    See https://medium.com/@alasher/colored-c-compiler-output-with-ninja-clang-gcc-10bfe7f2b949

--- modules/LiriSetup.cmake
+++ modules/LiriSetup.cmake
@@ -71,6 +71,16 @@ if(GCC)
     endif()
 endif()
 
+## Enable colored output:
+option(LIRI_FORCE_COLORED_OUTPUT "Always produce ANSI-colored output (gcc/Clang only)." ON)
+if(${LIRI_FORCE_COLORED_OUTPUT})
+    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+       add_compile_options("-fdiagnostics-color=always")
+    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+       add_compile_options("-fcolor-diagnostics")
+    endif()
+endif()
+
 ## Add Liri functions:
 include(LiriBuild)
 
